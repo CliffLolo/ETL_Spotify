4b1bb6943ed7be28ae17cb4612c452827b4630e7
clifflolo <cliffordfrimpong69@gmail.com>
2021-07-03 23:00:30 +0000

    updated etl script

--- Scripts/etl.py
+++ Scripts/etl.py
@@ -3,14 +3,13 @@ import requests
 import json
 import pandas as pd
 import datetime
-# from datetime import datetime
 from decouple import config
 import sqlite3
-# from sqlalchemy.orm import sessionmaker
-# import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+import sqlalchemy
 
 
-# DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 
 def data_validation(df):
     #check if the data frame is empty
@@ -88,28 +87,28 @@ if data_validation(songs_df):
 
 # Loading
 
-# engine = sqlalchemy.create_engine(DATABASE_LOCATION)
-# conn = sqlite3.connect('my_played_tracks.sqlite')
-# cursor = conn.cursor()
+engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+conn = sqlite3.connect('my_played_tracks.sqlite')
+cursor = conn.cursor()
 
-# sql_query = """
-# CREATE TABLE IF NOT EXISTS my_played_tracks(
-#     song_name VARCHAR(200),
-#     artist_name VARCHAR(200),
-#     played_at VARCHAR(200),
-#     timestamp VARCHAR(200),
-#     CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
-# )
-# """
+sql_query = """
+CREATE TABLE IF NOT EXISTS my_played_tracks(
+    song_name VARCHAR(200),
+    artist_name VARCHAR(200),
+    played_at VARCHAR(200),
+    timestamp VARCHAR(200),
+    CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+)
+"""
 
-# cursor.execute(sql_query)
-# print("Opened database successfully")
+cursor.execute(sql_query)
+print("Opened database successfully")
 
-# try:
-#     song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
-# except:
-#     print("Data already exists in the database")
+try:
+    song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+except:
+    print("Data already exists in the database")
 
-# conn.close()
-# print("Close database successfully")
+conn.close()
+print("Close database successfully")
     
