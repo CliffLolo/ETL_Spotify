2c97bd0bb833c76f039ed7dae72407e56e38aa34
clifflolo <cliffordfrimpong69@gmail.com>
2021-07-08 09:33:48 +0000

    added the pythonOperator

--- Scripts/dags/spotify_dag.py
+++ Scripts/dags/spotify_dag.py
@@ -1,3 +1,4 @@
+import datetime 
 from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -6,10 +7,26 @@ from airflow.utils.dates import days_ago
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020, 11, 8),
-    'email': ['airflow@example.com'],
+    'start_date': datetime(2021, 7, 8),
+    'email': ['cliffordfrimpong69@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=1)
 }
+
+dag = DAG(
+    'spotify_dag',
+    default_args=default_args,
+    description="This is my very first DAG. I'm getting there!",
+    schedule_interval=timedelta(days=30),
+)
+
+
+run_etl = PythonOperator(
+    task_id='final_spotify_etl',
+    python_callable=run_spotify_etl,
+    dag=dag,
+)
+
+run_etl
