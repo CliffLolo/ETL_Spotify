8747524a26c4a4219236af366b28ee011e13ba5f
clifflolo <cliffordfrimpong69@gmail.com>
2021-07-08 08:41:02 +0000

    updated libs

---
+++ Scripts/dags/spotify_dag.py
@@ -0,0 +1,15 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 11, 8),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
